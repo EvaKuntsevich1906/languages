23afbe8cf332372fd4d907f9f316253753b0d8da
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-11-01 00:07:56 +0300

    new components

--- ui.lang/src/App.js
+++ ui.lang/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import StartPage from './pages/StartPage/StartPage';
 import { Route, Routes } from 'react-router-dom'
 import RegisterPage from './pages/RegisterPage/RegisterPage';
+import AuthorizPage from './pages/AuthorizPage/AuthorizPage';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
      <Routes>
         <Route path='/' element={<StartPage></StartPage>}></Route>
         <Route path='/reg' element={<RegisterPage></RegisterPage>}></Route>
-
+        <Route path='/auth' element={<AuthorizPage></AuthorizPage>}></Route>
       </Routes>
 
     </div>
--- ui.lang/src/pages/AuthorizPage/AuthorizPage.js
+++ ui.lang/src/pages/AuthorizPage/AuthorizPage.js
@@ -2,13 +2,13 @@
 import style from './AuthorizPage.module.css';
 import Header from '../../components/Header/Header';
 import GirlWithPlanet from '../../components/GirlWithPlanet/GirlWithPlanet';
-
+import AuthorizForm from '../../components/AuthorizForm/AuthorizForm';
 const AuthorizPage = () => {
     return (
         <div className={style['authoriz-page']}>
             <Header></Header>
             <main>
-                <CreateForm></CreateForm>
+                <AuthorizForm></AuthorizForm>
                 <div className={style['image']}><GirlWithPlanet></GirlWithPlanet></div>
             </main>
 
